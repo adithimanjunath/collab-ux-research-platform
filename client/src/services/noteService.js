2a8c460de94ee4d3f7ae149d31c8ba088e3f93f3
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2025-07-28 00:46:11 +0200

    url updated

--- client/src/services/noteService.js
+++ client/src/services/noteService.js
@@ -1,13 +1,20 @@
+// const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5050";
+
+// export const fetchNotesByBoard = async (boardId) => {
+//   try {
+//     const res = await fetch(`${API_URL}/api/notes?boardId=${boardId}`);
+//     if (!res.ok) throw new Error(`HTTP ${res.status} - ${res.statusText}`);
+//     return await res.json();
+//   }
+//   catch (err) {
+//     console.error("❌ Failed to fetch notes:", err.message);
+//     throw err;
+//   }
+// };
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5050";
 
 export const fetchNotesByBoard = async (boardId) => {
-  try {
-    const res = await fetch(`${API_URL}/api/notes?boardId=${boardId}`);
-    if (!res.ok) throw new Error(`HTTP ${res.status} - ${res.statusText}`);
-    return await res.json();
-  }
-  catch (err) {
-    console.error("❌ Failed to fetch notes:", err.message);
-    throw err;
-  }
+  const res = await fetch(`${API_URL}/api/notes?boardId=${boardId}`);
+  if (!res.ok) throw new Error("Failed to fetch notes");
+  return await res.json();
 };
