028a924891d1fd1b52b6dfb221179e8692d5ae81
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2025-08-06 17:21:38 +0200

    added protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        
         <Route path="/collab"
           element={
             <ProtectedRoute>
