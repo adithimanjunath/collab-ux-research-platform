2cda67126444da35a9c6365d93000886136b5a8f
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2025-08-08 10:27:28 +0200

    firebase token issue-checking

--- server/auth/firebase_verify.py
+++ server/auth/firebase_verify.py
@@ -1,7 +1,20 @@
-
-from auth import firebase_config
+import firebase_admin
 from firebase_admin import auth as firebase_auth
+from firebase_admin import credentials
+import os, json
+from dotenv import load_dotenv
+from auth import firebase_config
 
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), 'server', '.env'))
+
+if not firebase_admin._apps:
+    cred_json = os.getenv("FIREBASE_CREDENTIAL_JSON")
+    cred_dict = json.loads(cred_json)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+    print("✅ Firebase initialized with:", cred_dict.get("project_id"))
+    
 def verify_firebase_token(token):
     try:
         return firebase_auth.verify_id_token(token)
