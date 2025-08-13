a81c03454d5d2bcad4c908df1244012d2f9d54a8
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2025-08-13 10:13:20 +0200

    docker compose update

--- server/db.py
+++ server/db.py
@@ -1,6 +1,5 @@
 import os
-from pymongo import MongoClient
-
+from pymongo import MongoClient, ASCENDING, errors
 # Use the value from Render's environment settings
 mongo_uri = os.environ.get("MONGO_URI")
 
@@ -12,3 +11,22 @@ client = MongoClient(mongo_uri)
 db = client["ux_research"]
 notes_collection = db["notes"]
 
+def ensure_indexes():
+    try:
+        # prevent duplicate UUIDs
+        notes_collection.create_index(
+            [("id", ASCENDING)],
+            unique=True,
+            name="uniq_note_id",
+        )
+    except errors.OperationFailure as e:
+        # This triggers if duplicates already exist
+        print("Couldn't create unique index on notes.id:", e)
+
+    # speed up board loads
+    notes_collection.create_index(
+        [("boardId", ASCENDING)],
+        name="idx_boardId",
+    )
+
+ensure_indexes()
